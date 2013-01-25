5d62ed9ab9d01c5082d0fc4f709ae87124d993ef
Ivan Necas <inecas@redhat.com>
2013-01-25 22:40:27 +0100

    First cut API documentation
    Extracted from `routes.rb` and functional tests by running:
    
        APIPIE_RECORD=params rake test:functionals
        APIPIE_RECORD=examples rake test:functionals

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,30 @@
 class TweetsController < ApplicationController
   before_filter :find_user
   
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :GET, "/users/:user_id/tweets", "List tweets"
   def index
     @tweets = @user.tweets.all
 
     render json: @tweets
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :GET, "/users/:user_id/tweets/:id", "Show a tweet"
   def show
     @tweet = @user.tweets.find(params[:id])
 
     render json: @tweet
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :POST, "/users/:user_id/tweets", "Create a tweet"
+  param :tweet, Hash do
+    param :scheduled_at, :undef
+    param :sent, :bool
+    param :text, :undef
+    param :user_id, :number
+  end
   def create
     @tweet = @user.tweets.new(params[:tweet])
 
@@ -23,6 +35,14 @@ class TweetsController < ApplicationController
     end
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :PUT, "/users/:user_id/tweets/:id", "Update a tweet"
+  param :tweet, Hash do
+    param :scheduled_at, :undef
+    param :sent, :bool
+    param :text, :undef
+    param :user_id, :number
+  end
   def update
     @tweet = @user.tweets.find(params[:id])
 
@@ -33,6 +53,8 @@ class TweetsController < ApplicationController
     end
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :DELETE, "/users/:user_id/tweets/:id", "Destroy a tweet"
   def destroy
     @tweet = @user.tweets.find(params[:id])
     @tweet.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :GET, "/users", "List users"
   def index
     @users = User.all
 
     render json: @users
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :GET, "/users/:id", "Show an user"
   def show
     @user = User.find(params[:id])
 
     render json: @user
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :POST, "/users", "Create an user"
+  param :user, Hash do
+    param :name, :undef
+  end
   def create
     @user = User.new(params[:user])
 
@@ -21,6 +30,11 @@ class UsersController < ApplicationController
     end
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :PUT, "/users/:id", "Update an user"
+  param :user, Hash do
+    param :name, :undef
+  end
   def update
     @user = User.find(params[:id])
 
@@ -31,6 +45,8 @@ class UsersController < ApplicationController
     end
   end
 
+  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
+  api :DELETE, "/users/:id", "Destroy an user"
   def destroy
     @user = User.find(params[:id])
     @user.destroy
